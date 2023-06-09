fce4cc422fd1e633ce1eda4a2c284155af572f06
Xiomara Sanchez <xiomalipa@gmail.com>
2023-06-08 18:32:08 -0600

    pruebagit1

---
+++ seccion12.js
@@ -0,0 +1,7 @@
+document.querySelector("button.button-menu-toggle")
+.addEventListener("click", function() {
+document.querySelector(".nav-links").
+classList.toggle("nav-links-responsive")})
+function showAlert()
+ { alert('Holi');
+}
